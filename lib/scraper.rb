a7cb61a2031d688c77b9ac2c0e30fb4c344076bf
aarongoen <aarongoen2.0@gmail.com>
2020-09-15 18:37:49 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,24 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  attr_reader :name, :location, :profile_url
+   
+    index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+    uri = URI.parse(url) 
+    
+  # def self.scrape_index_page(index_url)
+  #   doc = Nokogiri::HTML(open(index_url))
+    
+  #   name = doc.css()
+  #   location = doc.css()
+  #   profile_url = doc.css()
+
     
   end
+  # binding.pry
 
   def self.scrape_profile_page(profile_url)
     
